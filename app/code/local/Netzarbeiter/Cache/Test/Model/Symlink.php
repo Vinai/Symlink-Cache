05033fd24cf3cea3fa523a376e528850b2896c8a
Vinai Kopp <vinai@netzarbeiter.com>
2011-07-22 10:25:05 +0200

    Fix fixtures, add licence header to test class

--- app/code/local/Netzarbeiter/Cache/Test/Model/Symlink.php
+++ app/code/local/Netzarbeiter/Cache/Test/Model/Symlink.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension
+ * to newer versions in the future.
+ *
+ * @category   Netzarbeiter
+ * @package    Netzarbeiter_Cache
+ * @copyright  Copyright (c) 2011 Vinai Kopp http://netzarbeiter.com
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class Netzarbeiter_Cache_Test_Model_Symlink extends EcomDev_PHPUnit_Test_Case
 {
 
